74b31b2da7835dbb03e6bf36348b6b011f56d349
Home <tinchanisa@gmail.com>
2016-12-28 16:41:13 +0700

    Initial commit

---
+++ app/src/main/java/rtc/chanisa/kanokporn/massagethaitraining/MainActivity.java
@@ -0,0 +1,29 @@
+package rtc.chanisa.kanokporn.massagethaitraining;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,MainHub.class));
+                finish();
+            }
+        });
+
+    }
+
+}
