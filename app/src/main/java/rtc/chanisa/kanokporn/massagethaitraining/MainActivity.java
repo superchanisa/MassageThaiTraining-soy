d77b71368bfdb232d9e590efcab094594f1391e0
masterUNG <phrombutr@gmail.com>
2016-12-29 12:31:05 +0700

    เพิ่มหน้าแล้วนะ จรัา

--- app/src/main/java/rtc/chanisa/kanokporn/massagethaitraining/MainActivity.java
+++ app/src/main/java/rtc/chanisa/kanokporn/massagethaitraining/MainActivity.java
@@ -1,10 +1,9 @@
 package rtc.chanisa.kanokporn.massagethaitraining;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,MainHub.class));
+                startActivity(new Intent(MainActivity.this,Come.class));
                 finish();
             }
         });
